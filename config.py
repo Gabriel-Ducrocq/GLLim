9d4bac1fc8782b7473b846a7cd4a4c1771155b90
Gabriel Ducrocq <gabrielducrocq.maths@gmail.com>
2022-01-13 14:55:48 +0100

    test again

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ COSMO_PARAMS_NAMES = ["n_s", "omega_b", "omega_cdm", "100*theta_s", "ln10^{10}A_
 COSMO_PARAMS_MEAN_PRIOR = np.array([0.9665, 0.02242, 0.11933, 1.04101, 3.047, 0.0561])
 COSMO_PARAMS_SIGMA_PRIOR = np.array([0.0038, 0.00014, 0.00091, 0.00029, 0.014, 0.0071])
 proposal_covariance = np.array([0.0038, 0.00014, 0.00091, 0.00029, 0.014, 0.0071])**2
-
+print(proposal_covariance)
 nside = 256
 lmax = 2*nside
 npix = 12*nside**2
@@ -22,8 +22,10 @@ beam_fwhm = 0.5
 beam_fwhm_radians = (np.pi / 180) * beam_fwhm
 
 
-preliminary_run = True
+preliminary_run = False
 
 if not preliminary_run:
     preliminary_chain = np.load("data/mh_preliminary_run.npy")
     proposal_covariance = np.cov(preliminary_chain.T)
+    print("\n")
+    print(np.diag(proposal_covariance))
