150999f6370b9b3884ab39a2b36b28ea63cb8b70
Gabriel Ducrocq <gabrielducrocq.maths@gmail.com>
2022-01-13 11:15:31 +0100

    sending to labostat for 1000 iterations

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     like_approx.set_observed_cls(all_cls_tt_hat, all_cls_ee_hat, all_cls_te_hat)
     mh = metropolisHastings(like_approx)
     theta_init = np.random.normal(scale = config.COSMO_PARAMS_SIGMA_PRIOR) + config.COSMO_PARAMS_MEAN_PRIOR
-    N = 100
+    N = 1000
     start_time = time()
     res = mh.run(theta_init, N)
     end_time = time()
