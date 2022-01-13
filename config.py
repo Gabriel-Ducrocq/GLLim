75a7551111afa37430bbe4f67c176dc5b525fc53
Gabriel Ducrocq <gabrielducrocq.maths@gmail.com>
2022-01-13 15:35:27 +0100

    tuning

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ COSMO_PARAMS_NAMES = ["n_s", "omega_b", "omega_cdm", "100*theta_s", "ln10^{10}A_
 COSMO_PARAMS_MEAN_PRIOR = np.array([0.9665, 0.02242, 0.11933, 1.04101, 3.047, 0.0561])
 COSMO_PARAMS_SIGMA_PRIOR = np.array([0.0038, 0.00014, 0.00091, 0.00029, 0.014, 0.0071])
 proposal_covariance = np.array([0.0038, 0.00014, 0.00091, 0.00029, 0.014, 0.0071])**2
-print(proposal_covariance)
+
 nside = 256
 lmax = 2*nside
 npix = 12*nside**2
@@ -27,8 +27,3 @@ preliminary_run = False
 if not preliminary_run:
     preliminary_chain = np.load("data/mh_preliminary_run.npy")
     proposal_covariance = np.cov(preliminary_chain.T)
-    #np.fill_diagonal(proposal_covariance, np.array([0.0038, 0.00014, 0.00091, 0.00029, 0.014, 0.0071])**2)
-    #proposal_covariance = np.zeros((6, 6))
-    #np.fill_diagonal(proposal_covariance, np.array([0.0038, 0.00014, 0.00091, 0.00029, 0.014, 0.0071]) ** 2)
-    #print("\n")
-    #print(np.diag(proposal_covariance))
