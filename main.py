99dbdb5774c600596399514910608b8479d4fe3f
Gabriel Ducrocq <gabrielducrocq.maths@gmail.com>
2022-01-15 13:53:53 +0100

    trying a MH run of 1000

--- main.py
+++ main.py
@@ -20,37 +20,27 @@ if __name__ == "__main__":
 
     #regressor = forward_regressor(2)
     like_approx = likelihood()
-    all_cls_tt_hat, all_cls_ee_hat, all_cls_te_hat, all_theta =like_approx.generate_data(1)
-    np.save("data_true/cls_tt.npy", all_cls_tt_hat[0, :])
-    np.save("data_true/cls_ee.npy", all_cls_ee_hat[0, :])
-    np.save("data_true/cls_te.npy", all_cls_te_hat[0, :])
-    np.save("data_true/all_theta.npy", all_theta[0, :])
-
+    like_approx.generate_data(1)
 
     #all_cls_tt_hat, all_cls_ee_hat, all_cls_te_hat, all_theta = like_approx.generate_data(10000)
     #np.save("data/cls_tt.npy", all_cls_tt_hat)
     #np.save("data/cls_ee.npy", all_cls_ee_hat)
     #np.save("data/cls_te.npy", all_cls_te_hat)
     #np.save("data/all_theta.npy", all_theta)
-    #all_cls_tt_hat, all_cls_ee_hat, all_cls_te_hat, all_theta =like_approx.generate_data(1)
-    #np.save("data_true/cls_tt.npy", all_cls_tt_hat[0, :])
-    #np.save("data_true/cls_ee.npy", all_cls_ee_hat[0, :])
-    #np.save("data_true/cls_te.npy", all_cls_te_hat[0, :])
-    #np.save("data_true/all_theta.npy", all_theta[0, :])
 
-    #all_cls_tt_hat = np.load("data_true/cls_tt.npy")
-    #all_cls_ee_hat = np.load("data_true/cls_ee.npy")
-    #all_cls_te_hat = np.load("data_true/cls_te.npy")
-    #all_theta = np.load("data_true/all_theta.npy")
+    all_cls_tt_hat = np.load("data_true/cls_tt.npy")
+    all_cls_ee_hat = np.load("data_true/cls_ee.npy")
+    all_cls_te_hat = np.load("data_true/cls_te.npy")
+    all_theta = np.load("data_true/all_theta.npy")
 
-    #like_approx.set_observed_cls(all_cls_tt_hat, all_cls_ee_hat, all_cls_te_hat)
-    #mh = metropolisHastings(like_approx, cosmo_proposal_cov=config.proposal_covariance)
-    #theta_init = np.random.normal(scale = config.COSMO_PARAMS_SIGMA_PRIOR) + config.COSMO_PARAMS_MEAN_PRIOR
-    #N = 50000
-    #start_time = time()
-    #res = mh.run(theta_init, N)
-    #end_time = time()
-    #np.save("data/mh_official_run.npy", res)
+    like_approx.set_observed_cls(all_cls_tt_hat, all_cls_ee_hat, all_cls_te_hat)
+    mh = metropolisHastings(like_approx, cosmo_proposal_cov=config.proposal_covariance*0.5)
+    theta_init = np.random.normal(scale = config.COSMO_PARAMS_SIGMA_PRIOR) + config.COSMO_PARAMS_MEAN_PRIOR
+    N = 1000
+    start_time = time()
+    res = mh.run(theta_init, N)
+    end_time = time()
+    np.save("data/mh_official_run.npy", res)
 
 
 
