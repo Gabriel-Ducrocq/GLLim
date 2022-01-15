a023b9c1e6a1d1b788996ab0c8f5aa3f03036a4e
Gabriel Ducrocq <gabrielducrocq.maths@gmail.com>
2022-01-15 13:46:29 +0100

    again

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ beam_fwhm = 0.5
 beam_fwhm_radians = (np.pi / 180) * beam_fwhm
 
 
-preliminary_run = False
+preliminary_run = True
 
 if not preliminary_run:
     preliminary_chain = np.load("data/mh_preliminary_run.npy")
